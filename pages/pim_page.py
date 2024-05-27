f3bf01a81a1359e37e9640e45f4f958e4da7bcff
«Kirill» <kirillgryaznov_100@mail.ru>
2024-05-27 23:00:00 +0300

    Тест с добавлением сотрудника

--- pages/pim_page.py
+++ pages/pim_page.py
@@ -11,13 +11,37 @@ class PIMPage(BasePage):
     PAGE_URL = Links.PIM_PAGE
 
     CHECK_BOX = ("xpath", "(//div[@class='oxd-table-card-cell-checkbox'])[3]")
-    DELETED_SELECTED = ("xpath", "//button[@class='oxd-button oxd-button--medium oxd-button--label-danger orangehrm-horizontal-margin']")
+    DELETED_SELECTED_BUTTON = ("xpath", "//button[@class='oxd-button oxd-button--medium oxd-button--label-danger orangehrm-horizontal-margin']")
+    ADD_EMPLOYEE_BUTTON = ("xpath", "//a[text()='Add Employee']")
+    INPUT_FIRST_NAME = ("xpath", "//input[@placeholder='First Name']")
+    INPUT_MIDDLE_NAME = ("xpath", "//input[@placeholder='Middle Name']")
+    INPUT_LAST_NAME = ("xpath", "//input[@placeholder='Last Name']")
+    INPUT_EMPLOYEE_ID = ("xpath", "(//input[@class='oxd-input oxd-input--active'])[2]")
+    SAVE_BUTTON = ("xpath", "//button[text()=' Save ']")
 
     def click_check_boxes(self):
         self.wait.until(EC.element_to_be_clickable(self.CHECK_BOX)).click()
         time.sleep(2)
-        self.wait.until(EC.visibility_of_element_located(self.DELETED_SELECTED))
+        self.wait.until(EC.visibility_of_element_located(self.DELETED_SELECTED_BUTTON))
         print("Кнопка удаления появилась :)")
 
+    def click_add_employee(self):
+        self.wait.until(EC.element_to_be_clickable(self.ADD_EMPLOYEE_BUTTON)).click()
+
+    def input_first_name(self):
+        self.wait.until(EC.element_to_be_clickable(self.INPUT_FIRST_NAME)).send_keys('Кирилл')
+
+    def input_middle_name(self):
+        self.wait.until(EC.element_to_be_clickable(self.INPUT_MIDDLE_NAME)).send_keys('Александрович')
+
+    def input_last_name(self):
+        self.wait.until(EC.element_to_be_clickable(self.INPUT_LAST_NAME)).send_keys('Грязнов')
+
+    def input_employee_id(self):
+        self.wait.until(EC.element_to_be_clickable(self.INPUT_EMPLOYEE_ID)).send_keys('1999')
+
+
+    def click_save_button(self):
+        self.wait.until(EC.element_to_be_clickable(self.SAVE_BUTTON)).click()
 
 
--- tests/test_feature_profile.py
+++ tests/test_feature_profile.py
@@ -82,20 +82,36 @@ from base.base_taste import BaseTest
 #         self.pim_page.click_pim_link()
 #         self.pim_page.click_check_boxes()
 
+#
+# class TestTimePagePunch(BaseTest):
+#
+#     def test_time_page_punch(self):
+#         self.login_page.open()
+#         self.login_page.enter_login(self.data.LOGIN)
+#         self.login_page.enter_password(self.data.PASSWORD)
+#         self.login_page.click_submit_button()
+#         self.time_page.click_time_link()
+#         self.time_page.click_Attendance_select_button()
+#         self.time_page.click_punch_in_out_button()
+#         self.time_page.click_calendar_button()
+#         self.time_page.click_left_scroll_calendar_button()
+#         self.time_page.click_date_calendar()
+#         self.time_page.click_time_change_button()
+#         self.time_page.input_hour()
+#         self.time_page.click_in_button()
 
-class TestTimePagePunch(BaseTest):
 
-    def test_time_page_punch(self):
+class TestPimPageAddEmployee(BaseTest):
+
+    def test_add_employee(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
         self.login_page.enter_password(self.data.PASSWORD)
         self.login_page.click_submit_button()
-        self.time_page.click_time_link()
-        self.time_page.click_Attendance_select_button()
-        self.time_page.click_punch_in_out_button()
-        self.time_page.click_calendar_button()
-        self.time_page.click_left_scroll_calendar_button()
-        self.time_page.click_date_calendar()
-        self.time_page.click_time_change_button()
-        self.time_page.input_hour()
-        self.time_page.click_in_button()
+        self.time_page.click_pim_link()
+        self.pim_page.click_add_employee()
+        self.pim_page.input_first_name()
+        self.pim_page.input_middle_name()
+        self.pim_page.input_last_name()
+        self.pim_page.input_employee_id()
+        self.pim_page.click_save_button()
