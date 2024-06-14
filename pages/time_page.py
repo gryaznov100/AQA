1da623a6dab7033b190809e3c9bc4aeef1b9c29c
«Kirill» <kirillgryaznov_100@mail.ru>
2024-06-14 23:42:14 +0300

    Правка

--- pages/time_page.py
+++ pages/time_page.py
@@ -6,20 +6,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 class TimePage(BasePage):
-
     PAGE_URL = Links.TIME_PAGE
 
     ATTENDANCE_SELECT_BUTTON = ("xpath", "//span[text()='Attendance ']")
     PUNCH_IN_OUT_BUTTON = ("xpath", "//a[text()='Punch In/Out']")
     CALENDAR_BUTTON = ("xpath", "//i[@class='oxd-icon bi-calendar oxd-date-input-icon']")
-    LEFT_SCROLL_CALENDAR_BUTTON = ("xpath", "(//i[@class='oxd-icon bi-chevron-left'])[2]")
+    RIGHT_SCROLL_CALENDAR_BUTTON = ("xpath", "//i[@class='oxd-icon bi-chevron-right']")
     DATE_CALENDAR_BUTTON = ("xpath", "//div[text()='14']")
     TIME_CHANGE_BUTTON = ("xpath", "//i[@class='oxd-icon bi-clock oxd-time-input--clock']")
     HOUR_INPUT = ("xpath", "//input[@class='oxd-input oxd-input--active oxd-time-hour-input-text']")
+    IN_OUT_BUTTON = ("xpath", "//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")
     IN_BUTTON = ("xpath", "//button[text()=' In ']")
+    OUT_BUTTON = ("xpath", "//button[text()=' Out ']")
 
-
-    def click_Attendance_select_button(self):
+    def click_attendance_select_button(self):
         self.wait.until(EC.element_to_be_clickable(self.ATTENDANCE_SELECT_BUTTON)).click()
 
     def click_punch_in_out_button(self):
@@ -28,8 +28,8 @@ class TimePage(BasePage):
     def click_calendar_button(self):
         self.wait.until(EC.element_to_be_clickable(self.CALENDAR_BUTTON)).click()
 
-    def click_left_scroll_calendar_button(self):
-        self.wait.until(EC.element_to_be_clickable(self.LEFT_SCROLL_CALENDAR_BUTTON)).click()
+    def click_right_scroll_calendar_button(self):
+        self.wait.until(EC.element_to_be_clickable(self.RIGHT_SCROLL_CALENDAR_BUTTON)).click()
 
     def click_date_calendar(self):
         self.wait.until(EC.element_to_be_clickable(self.DATE_CALENDAR_BUTTON)).click()
@@ -41,9 +41,17 @@ class TimePage(BasePage):
         hour = self.wait.until(EC.element_to_be_clickable(self.HOUR_INPUT))
         hour.send_keys(Keys.COMMAND, "a")
         hour.send_keys(Keys.DELETE)
-        time.sleep(5)
+        # time.sleep(5)
         hour.send_keys("10")
 
     def click_in_button(self):
-        self.wait.until(EC.element_to_be_clickable(self.IN_BUTTON)).click()
+        # button = self.wait.until(EC.element_to_be_clickable(self.IN_BUTTON))
+        # button_in = self.wait.until(EC.visibility_of_element_located(self.IN_BUTTON))
+        # button_out = self.wait.until(EC.visibility_of_element_located(self.OUT_BUTTON))
+        # if button_in.is_dispayed() is True:
+        #     button_in.click()
+        # else:
+        #     button_out.click()
+
+        self.wait.until(EC.element_to_be_clickable(self.IN_OUT_BUTTON)).click()
         time.sleep(2)
--- tests/test_time_page.py
+++ tests/test_time_page.py
@@ -4,19 +4,18 @@ import pytest
 from base.base_taste import BaseTest
 
 
-
 class TestTimePagePunch(BaseTest):
-    @pytest.skip
+    # @pytest.skip
     def test_time_page_punch(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
         self.login_page.enter_password(self.data.PASSWORD)
         self.login_page.click_submit_button()
         self.time_page.click_time_link()
-        self.time_page.click_Attendance_select_button()
+        self.time_page.click_attendance_select_button()
         self.time_page.click_punch_in_out_button()
         self.time_page.click_calendar_button()
-        self.time_page.click_left_scroll_calendar_button()
+        self.time_page.click_right_scroll_calendar_button()
         self.time_page.click_date_calendar()
         self.time_page.click_time_change_button()
         self.time_page.input_hour()
